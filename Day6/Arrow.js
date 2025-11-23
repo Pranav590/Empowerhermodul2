e2040b08c6263a8a81f4597ed7eb0bf329505ee6
Pranav590 <pran9545gaikwad@gmail.com>
2025-11-23 18:10:32 +0530

    Updateed arrow function

--- Day6/Arrow.js
+++ Day6/Arrow.js
@@ -1,1 +1,3 @@
-const multiply = (a = 1, b = 1) =>a*b;
+const multiply = (a = 1, b = 1) => a* b;
+console.log(multiply(3, 4)); // Example → should print 12
+console.log(multiply());     // Example → should print 1
