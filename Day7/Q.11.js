eeeb404d457219013e3a7d078c00fe38565c97a6
Pranav590 <pran9545gaikwad@gmail.com>
2025-11-24 22:08:23 +0530

    Masai assignment map

---
+++ Day7/Q.11.js
@@ -0,0 +1,16 @@
+let products = [
+  { name: "Laptop", price: 1000 },
+  { name: "Mouse", price: 20 }
+];
+
+let names = products.map(function (item) {
+  return item.name;
+});
+
+products.forEach(function (item) {
+  if (item.price > 50) {
+    console.log(item.name + " is above $50");
+  } else {
+    console.log(item.name + " is below $50");
+ }
+});
